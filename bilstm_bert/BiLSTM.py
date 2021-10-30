c6d566712c01bcab00312abf7d76f73f2ae62c09
Alin Pisica <alinp2508@gmail.com>
2021-10-30 23:41:21 +0300

    BiLSTM + BERT

---
+++ bilstm_bert/BiLSTM.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+
+class BiLSTM(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, n_layers, 
+                 bidirectional, dropout):
+        
+        #Constructor
+        super().__init__()          
+        
+        #embedding layer
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        
+        #lstm layer
+        self.lstm = nn.LSTM(embedding_dim, 
+                           hidden_dim, 
+                           num_layers=n_layers, 
+                           bidirectional=bidirectional, 
+                           dropout=dropout,
+                           batch_first=True)
+        
+        #dense layer
+        self.fc = nn.Linear(hidden_dim * 2, output_dim)
+        
+        #activation function
+        # self.act = nn.Sigmoid()
+        # self.act = torch.argmax
+        
+    def forward(self, text, text_lengths):
+        
+        #text = [batch size,sent_length]
+        embedded = self.embedding(text)
+        #embedded = [batch size, sent_len, emb dim]
+      
+        #packed sequence
+        lengths = torch.as_tensor(text_lengths, dtype=torch.int64)
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, lengths, batch_first=True)
+        
+        packed_output, (hidden, cell) = self.lstm(packed_embedded)
+        #hidden = [batch size, num layers * num directions,hid dim]
+        #cell = [batch size, num layers * num directions,hid dim]
+        
+        #concat the final forward and backward hidden state
+        hidden = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1)
+                
+        #hidden = [batch size, hid dim * num directions]
+        dense_outputs=self.fc(hidden)
+
+        #Final activation function
+        # outputs=self.act(dense_outputs)
+        # outputs=dense_outputs.argmax(1)
+        
+        return dense_outputs
